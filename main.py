770dda0fa7f8c96d2764b43fe9878bfb71cfc2e9
farleyoli <farleyoli@hotmail.com>
2021-04-12 23:51:40 -0300

    Add scrollable image.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import *
 from PIL import ImageTk, Image
 
 class Application(tk.Frame):
@@ -9,23 +10,34 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.hi_there = tk.Button(self)
-        self.hi_there["text"] = "Hello World\n(click me)"
-        self.hi_there["command"] = self.put_image
-        self.hi_there.pack(side="top")
+        self.put_image()
 
-        self.quit = tk.Button(self, text="QUIT", fg="red",
-                              command=self.master.destroy)
-        self.quit.pack(side="bottom")
 
-    def say_hi(self):
-        print("hi there, everyone!")
+    def put_image(self, path = "test.jpeg"):
+        #image = ImageTk.PhotoImage(Image.open(path))
+        #label = tk.Label(image=image)
+        #label.image = image
+        #label.pack(side="bottom")
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
 
-    def put_image(self, path = "test.png"):
-        image = ImageTk.PhotoImage(Image.open(path))
-        label = tk.Label(image=image)
-        label.image = image
-        label.pack()
+        pre_image = Image.open(path)
+        self.image = ImageTk.PhotoImage(pre_image)
+
+        self.canvas = Canvas(self.master, bg="blue")
+        self.canvas.pack(expand='true', fill='both')
+
+        self.canvas.create_image(0, 0, anchor=NW, image=self.image)
+
+        #self.canvas.configure(yscrollcommand=self.vsb.set)
+        self.canvas.bind("<Left>",  lambda event: self.canvas.xview_scroll(-1, "units"))
+        self.canvas.bind("<Right>", lambda event: self.canvas.xview_scroll( 1, "units"))
+        self.canvas.bind("<Up>",    lambda event: self.canvas.yview_scroll(-1, "units"))
+        self.canvas.bind("<Down>",  lambda event: self.canvas.yview_scroll( 1, "units"))
+        self.canvas.focus_set()
+        self.canvas.bind("<1>", lambda event: self.canvas.focus_set())
+        self.canvas.configure(xscrollincrement='20')
+
 
 root = tk.Tk()
 app = Application(master=root)
